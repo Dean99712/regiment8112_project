c6c2e04928485b6987c135dc7e27b2ada89b5439
Dean Uziel <92694814+Dean99712@users.noreply.github.com>
2023-05-12 14:54:10 +0300

    colors gradiant

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,25 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:regiment8112_project/custom_text.dart';
 
+const topAlignment = Alignment.topLeft;
+const bottomAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.colors, {super.key});
+
+  final List<Color> colors;
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors: [
-              Colors.blueAccent,
-              Colors.blue,
-              Colors.cyan,
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          )),
-      child: const Center(
-        child: CustomText()
-        ),
+        colors: colors,
+        begin: topAlignment,
+        end: bottomAlignment,
+      )),
+      child: const Center(child: CustomText("Hello World!")),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'gradient_container.dart';
 
+final colors = [
+  Colors.blue.shade700,
+  Colors.blue,
+  Colors.cyan,
+  Colors.cyan.shade200,
+];
+
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Scaffold(
-      body: GradientContainer(),
+      body: GradientContainer(colors),
     ),
   ));
 }
