952700730c63bb3ead527e76d3a56fbc4cd8ace0
Dean Uziel <92694814+Dean99712@users.noreply.github.com>
2023-05-12 12:42:45 +0300

    Custom text

--- lib/custom_text.dart
+++ lib/custom_text.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 class CustomText extends StatelessWidget {
   const CustomText({super.key});
 
-
-
   @override
   Widget build(BuildContext context) {
     return const Text(
