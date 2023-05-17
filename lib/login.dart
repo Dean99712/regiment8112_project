786c1597bb0e2a5adfe221876e24f5ec06eab652
Dean Uziel <92694814+Dean99712@users.noreply.github.com>
2023-05-17 16:50:46 +0300

    Switch Screens

--- lib/login.dart
+++ lib/login.dart
@@ -23,11 +23,15 @@ class _LoginState extends State<Login> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Container(child: activeScreen == 'start-screen')
-            ? LoginPage(switchScreen)
-            : const GradientContainer(
-                [Color.fromRGBO(60, 58, 59, 1), Color.fromRGBO(60, 58, 59, 1)]),
-        // body: const LoginPage(),
+        body: Container(
+          child: activeScreen == 'start-screen'
+              ? LoginPage(switchScreen)
+              : const GradientContainer([
+                  Color.fromRGBO(60, 58, 59, 1),
+                  Color.fromRGBO(60, 58, 59, 1)
+                ]),
+          // body: const LoginPage(),
+        ),
       ),
     );
   }
