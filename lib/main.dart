80fa213627490d26c5d028a4387e862f4964b1af
deanuziel <dean2910997@gmail.com>
2023-07-05 18:46:50 -0700

    build for production

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'firebase_options.dart';
 void main() async {
 WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
+    name: 'Regiment8112',
     options: DefaultFirebaseOptions.currentPlatform,
   );
   Intl.defaultLocale = 'he';
